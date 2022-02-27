129f82777958f623492cb6dbc2cc803fc7969b69
rivaldo-maciel <rivaldomaciel@hotmail.com>
2022-02-27 14:18:36 -0300

    Adiciona Carousel

---
+++ src/components/ControlledCarousel/index.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import FirstImage from '../../assets/carousel-1.png';
+import SecondImage from '../../assets/carousel-2.jpg';
+import ThirdImage from '../../assets/carousel-3.png';
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={FirstImage}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={SecondImage}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src={ThirdImage}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ControlledCarousel;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header';
+import ControlledCarousel from '../../components/ControlledCarousel';
 
 function Home() {
   return (
-    <Header />
+    <>
+      <Header />
+      <ControlledCarousel />
+    </>
   );
 };
 
